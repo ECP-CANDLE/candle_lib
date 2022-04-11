60d7795df214259f32ae5399b416f29652c46b78
Harry Yoo <hsyoo@anl.gov>
2022-04-11 14:35:49 -0500

    add test cases

--- candle/tests/test_helper_utils.py
+++ candle/tests/test_helper_utils.py
@@ -7,13 +7,15 @@ import argparse
 import candle
 
 
-@pytest.mark.parametrize("varstr", ['YES', 'TRUE', 'T', 'Y', '1'])
+@pytest.mark.parametrize("varstr", ['YES', 'TRUE', 'T', 'Y', '1', 'Yes', 'yes', 'yeS', 'True', 'true', 'tRuE', 'TruE', 't', 'y'])
 def test_str2bool_true(varstr):
-    assert candle.str2bool(varstr) == True
+    assert candle.str2bool(varstr)
 
-@pytest.mark.parametrize("varstr", ['NO', 'FALSE', 'F', 'N', '0'])
+
+@pytest.mark.parametrize("varstr", ['NO', 'FALSE', 'F', 'N', '0', 'No', 'no', 'nO', 'False', 'false', 'fAlSe', 'f', 'n'])
 def test_str2bool_false(varstr):
-    assert candle.str2bool(varstr) == False
+    assert not candle.str2bool(varstr)
+
 
 def test_str2bool_exception():
     with pytest.raises(argparse.ArgumentTypeError):
