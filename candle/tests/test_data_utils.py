a73e47111871b9d87affc1b85b3ad0a5e86915d5
Harry Yoo <hsyoo@anl.gov>
2022-04-12 10:54:55 -0500

    add test case for data_utils. mostly skip

---
+++ candle/tests/test_data_utils.py
@@ -0,0 +1,93 @@
+"""
+Unit test for the data_tuils module.
+"""
+
+# import candle
+import pytest
+
+
+@pytest.mark.skip(reason="used by load_Xy_data_noheader")
+def test_to_categorical():
+    pass
+
+
+@pytest.mark.skip(reason="used by load_Xy_data2")
+def test_convert_to_class():
+    pass
+
+
+@pytest.mark.skip(reason="used by impute_and_scale_array")
+def test_scale_array():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="impute_and_scale_array is not used")
+def test_impute_and_scale_array():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_drop_impute_and_scale_dataframe():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_discretize_dataframe():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_discretize_array():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_lookup():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="used by p1b1 only")
+def test_load_X_data():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_load_X_data2():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="this function is not used")
+def test_load_Xy_one_hot_data():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="used by p1b2 only")
+def test_load_Xy_one_hot_data2():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="used by p1b2 only")
+def test_load_Xy_data2():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="used by tc1 only")
+def test_load_Xy_data_noheader():
+    pass
+
+
+# should we keep this?
+@pytest.mark.skip(reason="used by p1b1 only")
+def test_load_csv_data():
+    pass
