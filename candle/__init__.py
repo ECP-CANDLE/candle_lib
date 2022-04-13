fa00762e2dd93fc73af0c3fca7610fb3e41bbe99
crstngc <cristina.cgarcia@gmail.com>
2022-04-13 11:29:22 -0600

    Add benchmark, parsing utils and some tests

--- candle/__init__.py
+++ candle/__init__.py
@@ -1,6 +1,8 @@
 name = "candle"
 __version__ = '0.0.1'
 
+from .benchmark_def import Benchmark
+
 # import from data_utils
 from .data_utils import load_csv_data
 from .data_utils import load_Xy_one_hot_data2
@@ -13,17 +15,12 @@ from .file_utils import validate_file
 # import from generic_utils
 from .generic_utils import Progbar
 
-# import from viz_utils
-from .viz_utils import plot_history
-from .viz_utils import plot_scatter
-from .viz_utils import plot_array
-from .viz_utils import plot_density_observed_vs_predicted
-from .viz_utils import plot_2d_density_sigma_vs_error
-from .viz_utils import plot_histogram_error_per_sigma
-from .viz_utils import plot_decile_predictions
-from .viz_utils import plot_calibration_interpolation
-from .viz_utils import plot_calibrated_std
-from .viz_utils import plot_contamination
+# import from parsing_utils
+from .parsing_utils import ( ArgumentStruct,
+    finalize_parameters,
+    check_flag_conflicts,
+    parse_from_dictlist,
+)
 
 # import from helper_utils
 from .helper_utils import ( fetch_file,
@@ -45,6 +42,19 @@ from .feature_selection_utils import (
     select_features_by_variation,
     select_decorrelated_features,
 )
+
+# import from viz_utils
+from .viz_utils import plot_history
+from .viz_utils import plot_scatter
+from .viz_utils import plot_array
+from .viz_utils import plot_density_observed_vs_predicted
+from .viz_utils import plot_2d_density_sigma_vs_error
+from .viz_utils import plot_histogram_error_per_sigma
+from .viz_utils import plot_decile_predictions
+from .viz_utils import plot_calibration_interpolation
+from .viz_utils import plot_calibrated_std
+from .viz_utils import plot_contamination
+
 
 
 # import framework dependent utils
---
+++ candle/tests/test_benchmark.py
@@ -0,0 +1,83 @@
+"""
+Unit test for the benchmark module.
+"""
+
+import pytest
+
+
+import candle
+
+additional_definitions1 = [
+    {'name': 'base_lr',
+     'type': float,
+     'help': 'base learning rate'},
+]
+additional_definitions2 = [
+    {'name': 'latent_dim',
+     'type': int,
+     'help': 'latent dimensions'},
+    {'name': 'model',
+     'default': 'ae',
+     'choices': ['ae', 'vae', 'cvae'],
+     'help': 'model to use: ae,vae,cvae'},
+]
+required1 = ['loss', 'latent_dim', 'model']
+required2 = ['dropout']
+
+
+class SetupTest:
+    def __init__(self):
+        self.additional_definitions = additional_definitions1
+        self.required = required1
+        self.filepath = './'
+        self.defmodel = ''
+        self.framework = 'keras'
+        self.prog = 'fake_bmk'
+        self.desc = 'Fake benchmark'
+
+
+@pytest.fixture(scope="module")
+def testobj():
+    yield SetupTest()
+
+
+def test_benchmark_build(testobj):
+    try:
+        candle.Benchmark(testobj.filepath,
+                         testobj.defmodel,
+                         testobj.framework,
+                         testobj.prog,
+                         testobj.desc,
+                         )
+    except Exception as e:
+        print(e)
+        assert 0
+
+
+@pytest.mark.parametrize("required", [None, required1, required2])
+def test_benchmark_required(testobj, required):
+    class Benchmark2(candle.Benchmark):
+        def set_locals(self):
+            if required is not None:
+                self.required = set(required)
+            self.additional_definitions = testobj.additional_definitions
+
+
+@pytest.mark.parametrize("additional_definitions", [None, additional_definitions1, additional_definitions2])
+def test_benchmark_additional(testobj, additional_definitions):
+    class Benchmark2(candle.Benchmark):
+        def set_locals(self):
+            self.required = set(testobj.required)
+            if additional_definitions is not None:
+                self.additional_definitions = additional_definitions
+
+    try:
+        Benchmark2(testobj.filepath,
+                   testobj.defmodel,
+                   testobj.framework,
+                   testobj.prog,
+                   testobj.desc,
+                   )
+    except Exception as e:
+        print(e)
+        assert 0
