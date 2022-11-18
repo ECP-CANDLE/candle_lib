36c0b675884297f3d93a90efb63a2fe7d75160d9
Jamal <jamal@lanl.gov>
2022-11-18 14:02:29 -0700

    More pre-commit fixes

--- candle/__init__.py
+++ candle/__init__.py
@@ -4,10 +4,7 @@ __version__ = "0.0.1"
 # import framework dependent utils
 import sys
 
-from .benchmark_def import (
-    Benchmark,
-    create_params,
-)
+from .benchmark_def import Benchmark, create_params
 
 # import from data_preprocessing_utils
 from .data_preprocessing_utils import (
